bbc547d759b50954e4a4dfefc87c9e16f6463d97
Takanari Sasaki <takanari.s@hotmail.com>
2016-07-07 17:52:29 +0000

    Positive numbers with filter method

--- answers.js
+++ answers.js
@@ -12,3 +12,21 @@ function printPositives(numArray) {
 var numArray = [1,2,3,-1,4,-9,-10,3,-1,0];
 var positiveNumArray = printPositives(numArray);
 
+
+//Question 2: Positive numbers using filter method
+function isPositive2(num) {
+    if (num > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function getPositives(inputArrayNum) {
+    var filteredPositiveNum = inputArrayNum.filter(isPositive2);
+    return filteredPositiveNum;
+}
+
+var numArray2 = [2,5,7,-1,-2,5,-2,0];
+console.log(getPositives(numArray2));
